60e0ab2cf84b9312127f4fde1d032bc14e70f0b2
sadotu <nick.sarcasmo@gmail.com>
2012-11-13 00:42:12 +0000

    is_member.py toegevoegd

---
+++ is_member.py
@@ -0,0 +1,13 @@
+a = ["a", "b", 34, 4624, "dsaf"]
+x = "dsaf"
+y = 546
+
+def is_member(lst, value):
+	for item in lst:
+		#print item, value
+		if item == value:
+			return "yay!"
+	return "nope"
+
+print is_member(a, x)
+#print is_member(a, y)
