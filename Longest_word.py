5c638eb8716605f577b70310e906111d6cbcc69e
sadotu <nick.sarcasmo@gmail.com>
2012-11-14 22:32:31 +0000

    Longest_word.py gemaakt

---
+++ Longest_word.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("/home/nicker/Desktop/Python-exercises")
+import max_in_list
+
+
+def longest_word(lst):
+	chars = map(len, lst)
+	output = max_in_list.max_in_list(chars)
+	return output
+
+if __name__ == "__main__":
+	lst = ["hond", "kat", "olifant", "giraffe", "walvis", "krokodil", "sgbedbsdbwr"]
+	print longest_word(lst)
