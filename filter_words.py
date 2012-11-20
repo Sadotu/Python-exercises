0bcff35d14a150188d4ac515fe30d2fb0a777e24
Nick <nick.sarcasmo@gmail.com>
2012-11-20 13:48:39 +0100

    filter_words.py aangemaakt

---
+++ filter_words.py
@@ -0,0 +1,14 @@
+import random
+
+def filter_words(lst, n):
+	output = []
+	for item in lst:
+		print item
+		if item >= n:
+			output.append(item)
+	return n, output
+
+if __name__ == "__main__":
+	lst = ["hond", "kat", "olifant", "giraffe", "walvis", "krokodil", "sgbedbsdbwr"]
+	n = random.randint(0, 9)
+	print filter_words(lst, n)
--- overlapping.py
+++ overlapping.py
@@ -2,13 +2,13 @@ import sys
 sys.path.append("/media/Tardis/Python the hard way rawr/Python-exercises")
 import is_member
 
-lst1 = [2, 4, "gssr", 45, "hallo2"]
-lst2 = ["sg", 54, "hallo1", 2345, 2]
-
 def overlapping(lst1, lst2):
 	for item in lst2:
 		if	is_member.is_member(lst1, item):
 			return item
 	return None
 
-print overlapping(lst1, lst2)
+if __name__ == "__main__":
+	lst1 = [2, 4, "gssr", 45, "hallo2"]
+	lst2 = ["sg", 54, "hallo1", 2345, 2]
+	print overlapping(lst1, lst2)
